1891326a4a2a899bf2d2c93bf03bcd743609b02d
quinthemint <59623248+quinthemint@users.noreply.github.com>
2022-01-21 16:17:00 -0800

    Update app.java

--- app.java
+++ app.java
@@ -3,6 +3,11 @@ import javax.swing.event.SwingPropertyChangeSupport;
 public class app {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        useless();
     }
-    
+
+    public static void useless() {
+        System.out.println("This function does absolutely nothing.");
+        //but it wants to be rebased
+    }
 }
